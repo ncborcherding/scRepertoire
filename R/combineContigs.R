38da569d846d1ae0f3c37a5a79094f1203787481
Nick <nicholas-borcherding@uiowa.edu>
2020-01-26 12:11:08 -0600

    Update combineContigs.R
    Fixing issues with the B cell loading

--- R/combineContigs.R
+++ R/combineContigs.R
@@ -41,6 +41,8 @@ combineContigs <- function(df,
                 df[[i]] <- subset(df[[i]], chain != "Multi")
                 df[[i]] <- subset(df[[i]], chain == "IGH" | chain == "IGK" | chain == "IGL")
                 df[[i]] <- subset(df[[i]], productive == T | productive == "TRUE" | productive == "True")
+                df[[i]]$sample <- samples[i]
+                df[[i]]$ID <- ID[i]
             }
         }
 
@@ -79,18 +81,18 @@ combineContigs <- function(df,
                 } else if (length(location.i) == 3) { # if there are more than 2 chains for each cell
           if (is.na(data2[location.i[1],c("TCR1")])) { # if the 1st location is occupied by TRB
             Con.df[y,c("TCR2", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[1],c("TCR2", "cdr3", "cdr3_nt")] # add TRB info to the TCR2 related columns
-            if (is.na(data2[location.i[2],c("TCR1")])) { # if the 2nd location is occupied by TRB 
+            if (is.na(data2[location.i[2],c("TCR1")])) { # if the 2nd location is occupied by TRB
               TRdf <- paste(Con.df[y,c("TCR2", "cdr3_aa2", "cdr3_nt2")],data2[location.i[2],c("TCR2", "cdr3", "cdr3_nt")],sep=";") # paste TRBs from 1st and 2nd locations
               Con.df[y,c("TCR2", "cdr3_aa2", "cdr3_nt2")] <- TRdf # add the combination of 2 TRB chains to the TCR2 slot
-              Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- data2[location.i[3],c("TCR1", "cdr3", "cdr3_nt")] # add TRA from the 3rd location to the TCR1 related columns 
+              Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- data2[location.i[3],c("TCR1", "cdr3", "cdr3_nt")] # add TRA from the 3rd location to the TCR1 related columns
             } else { # if the 2nd location is occupied by TRA
-              Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- data2[location.i[1],c("TCR1", "cdr3", "cdr3_nt")] # add TRA info to the TCR1 related columns 
+              Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- data2[location.i[1],c("TCR1", "cdr3", "cdr3_nt")] # add TRA info to the TCR1 related columns
               if (is.na(data2[location.i[3],c("TCR1")])) { # if the 3rd location is occupied by TRB
                 TRdf <- paste(Con.df[y,c("TCR2", "cdr3_aa2", "cdr3_nt2")],data2[location.i[3],c("TCR2", "cdr3", "cdr3_nt")],sep=";") # paste TRB from 1st and 3rd locations
                 Con.df[y,c("TCR2", "cdr3_aa2", "cdr3_nt2")] <- TRdf # add combined TRB relatedn info to the TCR2 related columns
               } else { # if the 3rd location is occupied by TRA
                 TRdf <- paste(Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")],data2[location.i[3],c("TCR1", "cdr3", "cdr3_nt")],sep=";") # paste TRAs from 2nd and 3rd locations
-                Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- TRdf # Add combined TRA to TCR1 related columns 
+                Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- TRdf # Add combined TRA to TCR1 related columns
               }
             }
           } else { # if 1st location is occupied by TRA
@@ -101,12 +103,12 @@ combineContigs <- function(df,
                 Con.df[y,c("TCR2", "cdr3_aa2", "cdr3_nt2")] <- TRdf # add TRB combo to TCR2 related  columns
               } else { # if TRA is on 3rd location
                 TRdf <- paste(Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")],data2[location.i[3],c("TCR1", "cdr3", "cdr3_nt")],sep=";") # paste TRA from 1st and 3rd locations
-                Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- TRdf # add TRA combo to TCR1 related columns 
+                Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- TRdf # add TRA combo to TCR1 related columns
               }
             } else { # if TRA is on 2nd location
               TRdf <- paste(Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")],data2[location.i[2],c("TCR1", "cdr3", "cdr3_nt")],sep=";") # paste TRA from 1st and 2nd locations
               Con.df[y,c("TCR1", "cdr3_aa1", "cdr3_nt1")] <- TRdf # add the combination of 2 TRA chains to the TCR1 slot
-              Con.df[y,c("TCR2", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[3],c("TCR2", "cdr3", "cdr3_nt")] # add TRB to the TCR2 related columns 
+              Con.df[y,c("TCR2", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[3],c("TCR2", "cdr3", "cdr3_nt")] # add TRB to the TCR2 related columns
             }
           }
         } else if (length(location.i) == 1) {
@@ -147,28 +149,41 @@ combineContigs <- function(df,
                 location.i <- which(barcode.i == data2$barcode)
                 if (length(location.i) == 2){
                     if (is.na(data2[location.i[1],c("IGHct")])) {
-                        Con.df[y,c("IGLC", "cdr3_nt2", "cdr3_nt2")] <- data2[location.i[1],c("IGLct", "cdr3", "cdr3_nt")]
-                        Con.df[y,c("IGH", "cdr3_nt1", "cdr3_nt1")] <- data2[location.i[2],c("IGHct", "cdr3", "cdr3_nt")]
+                        Con.df[y,c("IGH", "cdr3_aa1", "cdr3_nt1")] <- data2[location.i[2],c("IGHct", "cdr3", "cdr3_nt")]
+                        if (is.na(data2[location.i[1],c("IGKct")])) {
+                            Con.df[y,c("IGLC", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[1],c("IGLct", "cdr3", "cdr3_nt")]
+                        }
+                        else {
+                            Con.df[y,c("IGLC", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[1],c("IGKct", "cdr3", "cdr3_nt")]
+                        }
                     } else {
                         Con.df[y,c("IGH", "cdr3_aa1", "cdr3_nt1")] <- data2[location.i[1],c("IGHct", "cdr3", "cdr3_nt")]
-                        Con.df[y,c("IGLC", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[2],c("IGLct", "cdr3", "cdr3_nt")]
+                        if (is.na(data2[location.i[1],c("IGKct")])) {
+                            Con.df[y,c("IGLC", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[2],c("IGLct", "cdr3", "cdr3_nt")]
+                        }
+                        else {
+                            Con.df[y,c("IGLC", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[1],c("IGKct", "cdr3", "cdr3_nt")]
+                        }
                     }
                 } else if (length(location.i) == 1) {
                     chain.i <- data2$chain[location.i]
                     if (chain.i == "IGH"){
                         Con.df[y,c("IGH", "cdr3_aa1", "cdr3_nt1")] <- data2[location.i[1],c("IGHct", "cdr3", "cdr3_nt")]
-                    } else {
+                    } else if (chain.i == "IGL") {
                         Con.df[y,c("IGLC", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[2],c("IGLct", "cdr3", "cdr3_nt")]
+                    }
+                    else {
+                        Con.df[y,c("IGLC", "cdr3_aa2", "cdr3_nt2")] <- data2[location.i[1],c("IGKct", "cdr3", "cdr3_nt")]
                     }
                 }
             }
             Con.df$CTgene <- paste(Con.df$IGH, Con.df$IGLC, sep="_")
-            Con.df$CTnt <- paste(Con.df$cdr3_nt_h, on.df$cdr3_nt_l, sep="_")
-            Con.df$CTaa <- paste(on.df$cdr3_h, Con.df$cdr3_l, sep="_")
-            Con.df$CTstrict <- paste(Con.df$IGH, Con.df$cdr3_nt_h, Con.df$IGLC, Con.df$cdr3_nt_l, sep="_")
+            Con.df$CTnt <- paste(Con.df$cdr3_nt1, Con.df$cdr3_nt2, sep="_")
+            Con.df$CTaa <- paste(Con.df$cdr3_aa1, Con.df$cdr3_aa2, sep="_")
+            Con.df$CTstrict <- paste(Con.df$IGH, Con.df$cdr3_nt1, Con.df$IGLC, Con.df$cdr3_nt2, sep="_")
             Con.df[Con.df == "NA_NA"] <- NA #remove the na when gene, aa, or nt is called later
             Con.df[Con.df == "NA_NA_NA_NA"] <- NA #remove the na when nt+gene is called later
-            data3 <- merge(data2, Con.df[,-which(names(Con.df) %in% c("IGH","IGLC"))], by = "barcode")
+            data3 <- merge(data2, Con.df, by = "barcode")
             data3 <- data3[, c("barcode", "sample", "ID", "IGH", "cdr3_aa1", "cdr3_nt1", "IGLC", "cdr3_aa2", "cdr3_nt2", "CTgene", "CTnt", "CTaa", "CTstrict")]
             final[[i]] <- data3
         }
@@ -178,7 +193,7 @@ combineContigs <- function(df,
         c <- paste(samples[i], "_", ID[i], sep="")
         names <- c(names, c)}
     names(final) <- names
-  
+
     for (i in seq_along(final)) {
     final[[i]] <- final[[i]][!duplicated(final[[i]]$barcode),]
   }
