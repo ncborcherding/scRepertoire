f3855e1cdbe9d78f5bb197a817b5f680e3590f71
Nick <nicholas-borcherding@uiowa.edu>
2021-05-17 08:21:16 -0500

    Update combineTRUST4.R

--- R/combineTRUST4.R
+++ R/combineTRUST4.R
@@ -40,7 +40,7 @@ combineTRUST4 <- function(df, samples = NULL, ID = NULL,
     
     chain1 <- cellT(cells)[[1]]
     chain2 <- cellT(cells)[[2]]
-    if (cell = "B") {
+    if (cell == "B") {
       chain1 <- "heavy"
       chain2 <- "light"
     }
