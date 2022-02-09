cafb70e6b562bca745e9fb317619ebf6d09a27df
theHumanBorch <ncborch@gmail.com>
2022-02-09 14:04:43 -0600

    Add clusterTCR single-cell

--- R/clusterTCR.R
+++ R/clusterTCR.R
@@ -2,11 +2,11 @@
 #'
 #' This function uses edit distances of either the nucleotide or amino acid 
 #' sequences of the CDR3 to cluster similar TCRs together. The distance clustering
-#' will then be ammended to the end of the list of combined contigs with the corresponding Vgene. The
-#' cluster will appear as CHAIN.num if a unique sequence or CHAIN:LD.num if clustered together.
-#' This function will only two chains recovered, multiple chains will automatically be reduced. This 
-#' function also underlies the combineBCR() function and therefore not needed for B cells. This may
-#' take some time to calculate the distances and cluster. 
+#' will then be amended to the end of the list of combined contigs. The cluster 
+#' will appear as CHAIN.num if a unique sequence or CHAIN:LD.num if clustered together.
+#' This function will only two chains recovered, multiple chains will automatically 
+#' be reduced. This function also underlies the combineBCR() function and therefore 
+#' not needed for B cells. This may take some time to calculate the distances and cluster. 
 #' 
 #' @examples
 # Getting the combined contigs
@@ -15,17 +15,19 @@
 #' 
 #' sub_combined <- clusterTCR(combined[[2]], chain = "TRA", sequence = "aa")
 #' 
-#' @param df The product of combineTCR() or exppression2List().
+#' @param df The product of combineTCR(), expression2List(), or combineExpression().
 #' @param chain The TCR to cluster - TRA, TRB, TRG, TRD
 #' @param sequence Clustering based on either "aa" or "nt"
 #' @param threshold The normalized edit distance to consider. The higher the number the more 
 #' similarity of sequence will be used for clustering.
-#' @param group.by The column header used for to calculate the cluster by.
+#' @param group.by The column header used for to calculate the cluster
 #' @importFrom stringdist stringdistmatrix
 #' @importFrom igraph graph_from_data_frame components
 #' @importFrom plyr join
 #' @importFrom dplyr bind_rows
 #' @importFrom stringr str_split
+#' @importFrom  rlang %||%
+#' @importFrom SummarizedExperiment colData<- colData
 #' @export
 #' @return List of clonotypes for individual cell barcodes
 
@@ -35,14 +37,24 @@ clusterTCR <- function(df,
                        threshold = 0.85, 
                        group.by = NULL) {
     output.list <- list()
-    df <- checkList(df)
+    dat <- list.input.return(df, group.by)
+    if (inherits(x=df, what ="Seurat") &&
+        !inherits(x=df, what ="SummarizedExperiment")) {
+      for(y in seq_along(dat)) {
+        dat[[y]]$cdr3_aa1 <- str_split(dat[[y]]$CTaa, "_", simplify = TRUE)[,1]
+        dat[[y]]$cdr3_aa2 <- str_split(dat[[y]]$CTaa, "_", simplify = TRUE)[,2]
+        dat[[y]]$cdr3_nt1 <- str_split(dat[[y]]$CTnt, "_", simplify = TRUE)[,1]
+        dat[[y]]$cdr3_nt2 <- str_split(dat[[y]]$CTnt, "_", simplify = TRUE)[,2]
+      }
+    }
+    dat <- checkList(dat)
     if(chain %in% c("TRA", "TRG")) {
         ref <- 1
     } else if(chain %in% c("TRB", "TRD")) {
         ref <- 2
     }
     ref2 <- paste0("cdr3_", sequence, ref)
-    bound <- bind_rows(df)
+    bound <- bind_rows(dat)
     #Should make it work as either grouped or non-grouped
     if (!is.null(group.by)) {
       bound <- split(bound, bound[,group.by])
@@ -98,14 +110,30 @@ clusterTCR <- function(df,
       colnames(output) <- c(colname, ref2)
       output.list[[x]] <- output
     }
-      for (i in seq_along(df)) {
-          tmp <- df[[i]]
+      for (i in seq_along(bound)) {
+          tmp <- bound[[i]]
           output <- bind_rows(output.list)
           tmp[,ref2] <- str_split(tmp[,ref2], ";", simplify = TRUE)[,1]
           output2 <- output[output[,2] %in% tmp[,ref2],]
           
-          tmp <-  suppressMessages(join(tmp,  output2))
-          df[[i]] <- tmp
+          tmp <-  unique(suppressMessages(join(tmp,  output2)))
+          bound[[i]] <- tmp
+      }
+    if(inherits(x=df, what ="Seurat") | inherits(x=df, what ="SummarizedExperiment")) {
+      PreMeta <- bind_rows(bound)
+      x <- colnames(PreMeta)[ncol(PreMeta)]
+      PreMeta <- as.data.frame(PreMeta[,x], row.names = PreMeta$barcode)
+      colnames(PreMeta) <- x
+      if (inherits(x=df, what ="Seurat")) { 
+        col.name <- names(PreMeta) %||% colnames(PreMeta)
+        df[[col.name]] <- PreMeta
+      } else {
+        rownames <- rownames(colData(df))
+        colData(df) <- cbind(colData(df), PreMeta[rownames,])[, union(colnames(colData(df)),  colnames(PreMeta))]
+        rownames(colData(df)) <- rownames 
+      }
+    } else {
+        df <- bound
       }
     return(df)
 }  
