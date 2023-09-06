45ae7f062aa5836624622be23893c4992c88905a
theHumanBorch <ncborch@gmail.com>
2023-09-06 15:07:10 -0500

    Update clonalOverlap
    Using switch function now for different indices and added cosine similarity

---
+++ R/clonalOverlap.R
@@ -0,0 +1,197 @@
+#' Examining the clonal overlap between groups or samples
+#'
+#' This functions allows for the calculation and visualizations of the 
+#' overlap coefficient, morisita, or jaccard index for clonotypes 
+#' using the product of combineTCR(), combineBCR() or expression2list(). 
+#' The overlap coefficient is calculated using the intersection of clonotypes 
+#' divided by the length of the smallest component. 
+#' If a matrix output for the data is preferred, set exportTable = TRUE.
+#'
+#' @examples
+#' #Making combined contig data
+#' combined <- combineTCR(contig_list, 
+#'                         samples = c("P17B", "P17L", "P18B", "P18L", 
+#'                                     "P19B","P19L", "P20B", "P20L"))
+#' 
+#' clonalOverlap(combined, cloneCall = "gene", method = "overlap")
+#'
+#' @param df The product of combineTCR(), combineBCR(), expression2List(), or combineExpression().
+#' @param cloneCall How to call the clonotype - VDJC gene (gene), 
+#' CDR3 nucleotide (nt), CDR3 amino acid (aa), or 
+#' VDJC gene + CDR3 nucleotide (strict).
+#' @param chain indicate if both or a specific chain should be used - 
+#' e.g. "both", "TRA", "TRG", "IGH", "IGL"
+#' @param method The method to calculate the overlap, either the "overlap" 
+#' coefficient, "morisita", "jaccard" indices, "cosine" similarity or "raw" 
+#' for the base numbers.
+#' @param split.by If using a single-cell object, the column header 
+#' to group the new list. NULL will return clusters.
+#' @param exportTable Returns the data frame used for forming the graph
+#' @importFrom stringr str_sort
+#' @importFrom reshape2 melt
+#' @export
+#' @return ggplot of the clonotypic overlap between elements of a list
+clonalOverlap <- function(df, 
+                          cloneCall = "strict", 
+                          method = c("overlap", "morisita", "jaccard", "raw"), 
+                          chain = "both", 
+                          split.by = NULL, 
+                          exportTable = FALSE){
+    df <- list.input.return(df, split.by)
+    cloneCall <- theCall(cloneCall)
+    df <- checkBlanks(df, cloneCall)
+    df <- df[order(names(df))]
+    values <- str_sort(as.character(unique(names(df))), numeric = TRUE)
+    df <- df[quiet(dput(values))]
+    num_samples <- length(df[])
+    names_samples <- names(df)
+    coef_matrix <- data.frame(matrix(NA, num_samples, num_samples))
+    colnames(coef_matrix) <- names_samples
+    rownames(coef_matrix) <- names_samples
+    length <- seq_len(num_samples)
+    if (chain != "both") {
+      for (i in seq_along(df)) {
+        df[[i]] <- off.the.chain(df[[i]], chain, cloneCall)
+      }
+    }
+    
+    coef_matrix <- switch(method,
+                     "cosine" = .cosineIndex(df, length, cloneCall, coef_matrix),
+                     "jaccard" = .jaccardIndex(df, length, cloneCall, coef_matrix),
+                     "morisita" = .morisitaIndex(df, length, cloneCall, coef_matrix),
+                     "overlap" = .overlapIndex(ddf, length, cloneCall, coef_matrix),
+                     "raw" = .rawIndex(df, length, cloneCall, coef_matrix),
+                     "Invalid method specified")
+    coef_matrix$names <- rownames(coef_matrix)
+    if (exportTable == TRUE) { return(coef_matrix) }
+    coef_matrix <- suppressMessages(melt(coef_matrix))[,-1]
+    col <- colorblind_vector(7)
+    coef_matrix$variable <- factor(coef_matrix$variable, levels = values)
+    coef_matrix$names <- factor(coef_matrix$names, levels = values)
+    plot <- ggplot(coef_matrix, aes(x=names, y=variable, fill=value)) +
+            geom_tile() + labs(fill = method) +
+            geom_text(aes(label = round(value, digits = 3))) +
+            scale_fill_gradientn(colors = rev(colorblind_vector(5)), na.value = "white") +
+            theme_classic() + theme(axis.title = element_blank())
+    return(plot) 
+}
+
+#Calculate the Morisita Index for Overlap Analysis
+#' @author Massimo Andreatta, Nick Borcherding
+.morisitaIndex <- function(df, length, cloneCall, coef_matrix) {
+  for (i in seq_along(length)){
+    df.i <- df[[i]]
+    df.i <- data.frame(table(df.i[,cloneCall]))
+    colnames(df.i) <- c(cloneCall, 'Count')
+    df.i[,2] <- as.numeric(df.i[,2])
+    for (j in seq_along(length)){
+      if (i >= j){ next }
+      else { df.j <- df[[j]]
+      df.j <- data.frame(table(df.j[,cloneCall]))
+      colnames(df.j) <- c(cloneCall, 'Count')
+      df.j[,2] <- as.numeric(df.j[,2])
+      merged <- merge(df.i, df.j, by = cloneCall, all = TRUE)
+      merged[is.na(merged)] <- 0
+      X <- sum(merged[,2])
+      Y <- sum(merged[,3])
+      sum.df.i <- sum(df.i[,2]^2)
+      sum.df.j <- sum(df.j[,2]^2)
+      
+      num <- 2 * sum(merged[, 2] * merged[, 3])
+      den <- ((sum.df.i / (X^2) + sum.df.j / (Y^2)) * X * Y)
+      
+      coef.i.j <- num/den
+      coef_matrix[i,j] <- coef.i.j
+      }
+    }
+  }
+  return(coef_matrix)
+}
+
+
+#Calculate the Jaccard Similarity Index for Overlap Analysis
+.jaccardIndex <- function(df, length, cloneCall, coef_matrix) {
+  for (i in seq_along(length)){
+    df.i <- df[[i]]
+    df.i <- df.i[,c("barcode",cloneCall)]
+    df.i_unique <- df.i[!duplicated(df.i[,cloneCall]),]
+    for (j in seq_along(length)){
+      if (i >= j){ next }
+      
+      df.j <- df[[j]]
+      df.j <- df.j[,c("barcode",cloneCall)]
+      df.j_unique <- df.j[!duplicated(df.j[,cloneCall]),]
+      overlap <- length(intersect(df.i_unique[,cloneCall], 
+                                  df.j_unique[,cloneCall]))
+      coef_matrix[i,j] <- 
+        overlap/(sum(length(df.i_unique[,cloneCall]), 
+                     length(df.j_unique[,cloneCall]))-overlap)
+    }
+  }
+  return(coef_matrix)
+}
+
+.rawIndex <- function(df, length, cloneCall, coef_matrix) {
+  for (i in seq_along(length)){
+    df.i <- df[[i]]
+    df.i <- df.i[,c("barcode",cloneCall)]
+    df.i_unique <- df.i[!duplicated(df.i[,cloneCall]),]
+    for (j in seq_along(length)){
+      if (i >= j){ next }
+      df.j <- df[[j]]
+      df.j <- df.j[,c("barcode",cloneCall)]
+      df.j_unique <- df.j[!duplicated(df.j[,cloneCall]),]
+      overlap <- length(intersect(df.i_unique[,cloneCall], 
+                                  df.j_unique[,cloneCall]))
+      coef_matrix[i,j] <- overlap
+    }
+  }
+  return(coef_matrix)
+}
+
+
+#Calculate the Overlap Coefficient for Overlap Analysis
+#' @author Nick Bormann, Nick Borcherding
+.overlapIndex <- function(df, length, cloneCall, coef_matrix) {
+  for (i in seq_along(length)){
+    df.i <- df[[i]]
+    df.i <- df.i[,c("barcode",cloneCall)]
+    df.i_unique <- df.i[!duplicated(df.i[,cloneCall]),]
+    for (j in seq_along(length)){
+      if (i >= j){ next }
+      else { df.j <- df[[j]]
+      df.j <- df.j[,c("barcode",cloneCall)]
+      df.j_unique <- df.j[!duplicated(df.j[,cloneCall]),]
+      overlap <- length(intersect(df.i_unique[,cloneCall], 
+                                  df.j_unique[,cloneCall]))
+      coef_matrix[i,j] <- 
+        overlap/min(length(df.i_unique[,cloneCall]), 
+                    length(df.j_unique[,cloneCall])) } } }
+  return(coef_matrix)
+}
+
+.cosineIndex <- function(df, length, cloneCall, coef_matrix) {
+  for (i in seq_along(length)){
+    df.i <- df[[i]]
+    df.i <- df.i[,c("barcode",cloneCall)]
+    df.i_unique <- df.i[!duplicated(df.i[,cloneCall]),]
+    for (j in seq_along(length)){
+      if (i >= j){ next }
+      else { 
+        df.j <- df[[j]]
+        df.j <- df.j[,c("barcode",cloneCall)]
+        df.j_unique <- df.j[!duplicated(df.j[,cloneCall]),]
+        all_species <- unique(c(df.j_unique[,cloneCall], df.j_unique[,cloneCall]))
+        vector_location1 <- as.integer(all_species %in% df.i_unique[,cloneCall])
+        vector_location2 <- as.integer(all_species %in% df.j_unique[,cloneCall])
+        
+        coef_matrix[i,j] <- 
+          sum(vector_location1 * vector_location2) / (sqrt(sum(vector_location1^2)) * sqrt(sum(vector_location2^2)))
+      } 
+    } 
+  }
+  coef_matrix <- as.matrix(coef_matrix)
+  coef_matrix[is.nan(coef_matrix)] <- 0
+  return(coef_matrix)
+}
+
