4afc3214cf8978ac3f5a363ac29272e6ef0838a6
theHumanBorch <ncborch@gmail.com>
2023-09-06 13:37:14 -0500

    finished percentAA

--- R/percentAA.R
+++ R/percentAA.R
@@ -1,4 +1,14 @@
-
+#' Examining the relative amino acid composition by position
+#'
+#' This function the proportion of amino acids along the residues 
+#' of the cdr3 amino acid sequence.
+#'
+#' @examples
+#' #Making combined contig data
+#' combined <- combineTCR(contig_list, 
+#'                         samples = c("P17B", "P17L", "P18B", "P18L", 
+#'                                     "P19B","P19L", "P20B", "P20L"))
+#' percentAA(combined, chain = "TRB", aa.length = 20)
   
 #' @param df The product of combineTCR(), combineBCR(), 
 #' expression2List(), or combineExpression().
@@ -6,28 +16,28 @@
 #' @param group.by The column header used for grouping.
 #' @param split.by If using a single-cell object, the column header 
 #' to group the new list. NULL will return clusters.
-#' @param order Maintain the order of the list when plotting
-#' @param scale Converts the graphs into percentage of unique clonotypes.
+#' @param aa.length The maximum length of the cdr3 amino acid sequence 
 #' @param exportTable Returns the data frame used for forming the graph
 #' @import ggplot2
 #' @export
-#' @return ggplot of the total or relative unique clonotypes
+#' @return ggplot of stacked bar graphs of amino acid proportions
 percentAA <- function(df, 
                         chain = "TRB", 
                         group.by = NULL, 
                         split.by = NULL,
+                        aa.length = 30,
                         exportTable = FALSE) 
   df <- list.input.return(df, split.by)
   df <- checkBlanks(df, "CTaa")
   for(i in seq_along(df)) {
       df[[i]] <- off.the.chain(df[[i]], chain, "CTaa")
   }
-  
+  res.list <- list()
   for (i in seq_along(df)) {
     strings <- df[[i]][,"CTaa"]
     strings <- do.call(c,str_split(strings, ";"))
     strings <- strings[strings != "NA"]
-    aa.length <- max(nchar(strings))
+    strings <- strings[nchar(strings) < aa.length]
     strings <- .padded_strings(strings, aa.length)
     strings <- do.call(rbind, strings)
     
@@ -40,11 +50,24 @@ percentAA <- function(df,
     res <- suppressWarnings(Reduce(function(...) merge(..., all = TRUE, by="x"), aa.output))
     colnames(res) <- c("AA", paste0("pos.", seq_len(aa.length)))
     res[1:20,][is.na(res[1:20,])] <- 0
-    ######
-    #Need to come up with a summary function
+    melt.res <- suppressMessages(melt(res))
+    melt.res$group <- names(df)[i]
+    res.list[[i]] <- melt.res
   }
-  #Need to come up with a visualization
-    
+  mat_melt <- do.call(rbind, res.list)
+  plot <- ggplot(mat_melt, aes(x=as.factor(variable), y = value, fill=AA)) +
+    geom_bar(stat = "identity", position="fill", lwd= 0.25, color = "black") +
+    scale_fill_manual(name = "Amino Acid", 
+                      values = rev(colorblind_vector(21))) +
+    xlab("Amino Acid Residues") +
+    ylab("Relative Percent") +
+    theme_classic() + 
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  if(length(res.list) > 1) {
+    plot <- plot + facet_grid(group~.)
+  }
+  if (exportTable == TRUE) { return(mat_melt) }
+  return(plot)
 }    
     
 .padded_strings <- function(strings, max.length) {
