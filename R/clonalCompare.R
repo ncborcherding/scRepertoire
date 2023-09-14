b73bc9a68d3d9c595d061a936ab4b91da21a71a2
theHumanBorch <ncborch@gmail.com>
2023-09-14 05:21:58 -0500

    clonalCompare

--- R/clonalCompare.R
+++ R/clonalCompare.R
@@ -9,22 +9,27 @@
 #' @examples
 #' #Making combined contig data
 #' combined <- combineTCR(contig_list, 
-#'                         samples = c("P17B", "P17L", "P18B", "P18L", 
-#'                                     "P19B","P19L", "P20B", "P20L"))
-#' compareClonotypes(combined, 
-#'                   numbers = 5, 
-#'                   samples = c("PX_P", "PX_T"), 
-#'                   cloneCall="aa")
+#'                        samples = c("P17B", "P17L", "P18B", "P18L", 
+#'                                    "P19B","P19L", "P20B", "P20L"))
+#' clonalCompare(combined, 
+#'               top.clones = 5, 
+#'               samples = c("P17B", "P17L"), 
+#'               cloneCall="aa")
 #'
-#' @param df The product of combineTCR(), combineBCR(), expression2List(), or combineExpression().
+#' @param df The product of combineTCR(), combineBCR(), expression2List(), 
+#' or combineExpression().
 #' @param cloneCall How to call the clonotype - VDJC gene (gene), 
 #' CDR3 nucleotide (nt), CDR3 amino acid (aa), or 
 #' VDJC gene + CDR3 nucleotide (strict).
 #' @param chain indicate if both or a specific chain should be used - 
 #' e.g. "both", "TRA", "TRG", "IGH", "IGL"
 #' @param samples The specific samples to isolate for visualization.
-#' @param clonotypes The specific sequences of interest.
-#' @param numbers The top number clonotype sequences per group
+#' @param clonotypes The specific clonal sequences of interest.
+#' @param top.clones The top number of clonotype sequences per group
+#' @param highlight.clones Clonal sequences to highlight, if present, 
+#' all other clones returned will be grey.
+#' @param relabel.clones Simplify the legend of the graph by returning
+#' clones that are numerically indexed.
 #' @param split.by If using a single-cell object, the column header 
 #' to group the new list. NULL will return clusters.
 #' @param graph The type of graph produced, either "alluvial" or "area".
@@ -35,23 +40,30 @@
 #' @export
 #' @return ggplot of the proportion of total sequencing read of 
 #' selecting clonotypes
-compareClonotypes <- function(df, 
+clonalCompare <- function(df, 
                               cloneCall = "strict", 
                               chain = "both", 
                               samples = NULL, 
                               clonotypes = NULL, 
-                              numbers = NULL, 
+                              top.clones = NULL,
+                              highlight.clones = NULL,
+                              relabel.clones = FALSE,
                               split.by = NULL, 
                               graph = "alluvial", 
                               exportTable = FALSE, 
                               palette = "inferno"){
+  
+  #Tie goes to indicated clones over top clones
+  if(!is.null(top.clones) & !is.null(clonotypes)) {
+    top.clones <- NULL
+  }
+  
   df <- list.input.return(df, split.by)
   cloneCall <- theCall(cloneCall)
   df <- checkBlanks(df, cloneCall)
-  if (!is.null(numbers) & !is.null(clonotypes)) {
-    stop("Make sure your inputs are either numbers or clonotype sequences.")
-  }
   Con.df <- NULL
+  
+  #Loop through the list to get a proportional summary
   for (i in seq_along(df)) {
     if (chain != "both") {
       df[[i]] <- off.the.chain(df[[i]], chain, cloneCall)
@@ -62,34 +74,63 @@ compareClonotypes <- function(df,
     tbl$Sample <- names(df[i])
     Con.df <- rbind.data.frame(Con.df, tbl)
   }
+  
+  #Filtering steps 
   if (!is.null(samples)) {
     Con.df <- Con.df[Con.df$Sample %in% samples,] 
   }
   if (!is.null(clonotypes)) {
     Con.df <- Con.df[Con.df$Clonotypes %in% clonotypes,] 
-  }
-  if (!is.null(numbers)) {
+  } else if (!is.null(top.clones)) {
     top <- Con.df %>%
       group_by(Con.df[,3]) %>%
-      slice_max(n = numbers, order_by = Proportion, with_ties = FALSE)
+      slice_max(n = top.clones, order_by = Proportion, with_ties = FALSE)
     Con.df <- Con.df[Con.df$Clonotypes %in% top$Clonotypes,] 
   }
   if (nrow(Con.df) < length(unique(Con.df$Sample))) {
     stop("Reasses the filtering strategies here, there is not 
-            enough clonotypes to examine.") }
-  if (exportTable == TRUE) { return(Con.df)}
+            enough clonotypes to examine.") 
+  }
+  #Clonotype relabeling
+  clones.returned <- as.vector(unique(Con.df[,"Clonotypes"]))
+  if (relabel.clones) {
+    new.clones <- paste0("Clonotype: ", seq_len(length(clones.returned)))
+    names(new.clones) <- clones.returned
+    #Isolated new clone names for highlight purposes
+    if(!is.null(highlight.clones)) {
+      highlight.clones <- unname(new.clones[which(names(new.clones) %in% highlight.clones)])
+    }
+    Con.df[,"Clonotypes"] <- new.clones[as.vector(Con.df[,"Clonotypes"])]
+    clones.returned <- as.vector(unique(Con.df[,"Clonotypes"]))
+  }
+  if (exportTable == TRUE) { 
+    return(Con.df)
+  }
   
+  #Plotting Functions
   plot <- ggplot(Con.df, aes(x = Sample, fill = Clonotypes, group = Clonotypes,
                              stratum = Clonotypes, alluvium = Clonotypes, 
                              y = Proportion, label = Clonotypes)) +
     theme_classic() +
-    theme(axis.title.x = element_blank())
+    theme(axis.title.x = element_blank(), 
+          legend.text=element_text(size=rel(0.5)), 
+          legend.key.size = unit(0.5,"line"))
   if (graph == "alluvial") {
     plot <- plot +  geom_stratum() + geom_flow(stat = "alluvium")
   } else if (graph == "area") {
     plot <- plot +
       geom_area(aes(group = Clonotypes), color = "black")
   }
-  plot <- plot + scale_fill_manual(values = .colorizer(palette, length(unique(Con.df[,"Clonotypes"]))))
+  
+  #Highlighting specific clones
+  if (!is.null(highlight.clones)) {
+    clone.colors <- rep("grey", length(clones.returned))
+    pos <- which(clones.returned %in% highlight.clones)
+    clone.colors[pos] <- .colorizer(palette, length(pos))
+    names(clone.colors) <- clones.returned
+    plot <- plot + scale_fill_manual(values = clone.colors)
+  } else {
+    plot <- plot + scale_fill_manual(values = .colorizer(palette, length(unique(Con.df[,"Clonotypes"]))))
+  }
   return(plot)
 }
