cc17499582e2ea1f37585c1026f1b8e0fe58205e
Qile0317 <qile0317@gmail.com>
2024-10-14 02:18:40 -0700

    improve getContigDoublets and prelim test

--- R/getContigDoublets.R
+++ R/getContigDoublets.R
@@ -2,79 +2,66 @@
 #'
 #' @description
 #' `r lifecycle::badge("experimental")`
-#' 
+#'
 #' This function identifies potential doublets by finding common barcodes
 #' between TCR and BCR outputs. It extracts unique barcodes from each list
 #' of dataframes, finds the intersection of the barcodes, and joins the
 #' resulting data.
 #'
-#' @param tcrOutput Output of [combineTCR()]. A list of data.frames containing TCR contig
-#' information, each dataframe must have a `barcode` column.
-#' @param bcrOutput Output of [combineBCR()]. A list of data.frames containing BCR contig
-#' information, each dataframe must have a `barcode` column.
+#' @param tcrOutput Output of [combineTCR()]. A list of data.frames containing
+#' TCR contig information, each dataframe must have a `barcode` column.
+#' @param bcrOutput Output of [combineBCR()]. A list of data.frames containing
+#' BCR contig information, each dataframe must have a `barcode` column.
 #'
-#' @return A dataframe of barcodes that exist in both the TCR and BCR data, with
-#' columns from both sets of data. If there are no doublets, the returned
+#' @return
+#' A dataframe of barcodes that exist in both the TCR and BCR data, with
+#' columns from both sets of data. There will be an additional column `contigType`
+#' of type factor with levels 'TCR' and 'BCR' indicating the origin of the contig.
+#'
+#' If there are no doublets, the returned
 #' data.frame will have the same colnames but no rows.
+#'
 #' @export
-#' @examples
-#' # TODO
 getContigDoublets <- function(tcrOutput, bcrOutput) {
 
     assert_that(isListOfNonEmptyDataFrames(tcrOutput))
+    assert_that(all(sapply(tcrOutput, function(d) "barcode" %in% colnames(d))))
     assert_that(isListOfNonEmptyDataFrames(bcrOutput))
+    assert_that(all(sapply(bcrOutput, function(d) "barcode" %in% colnames(d))))
 
-    rawBarcodeColname <- tempColnameForDfList(
-        c(tcrOutput, bcrOutput), "raw_barcode"
-    )
-
-    listOfTcrBcrWithRawBarcode <- list(tcrOutput, bcrOutput) %>%
-        lapplyOnAll(function(df) {
-            df[[rawBarcodeColname]] <- extractBarcodeStrings(df$barcode)
-            df
-        })
-
-    doubletBarcodes <- listOfTcrBcrWithRawBarcode %>%
-        lapplyOnAll(function(df) {
-            unique(df[[rawBarcodeColname]])
-        }) %>%
-        lapply(purrr::list_flatten) %>%
-        purrr::reduce(intersect)
+    doubletBarcodes <- getContigDoubletBarcodes(tcrOutput, bcrOutput)
 
     if (length(doubletBarcodes) == 0) {
-        return(makeEmptyIntersectionDf(tcrOutput[[1]], bcrOutput[[1]]))
+        output <- autoFullJoin(tcrOutput[[1]][0, ], bcrOutput[[1]][0, ])
+        output$contigType <- factor(character(0), levels = c("BCR", "TCR"))
+        return(output)
     }
 
-    listOfTcrBcrWithRawBarcode %>%
+    RbindedTcrBcrDoublets <- list(tcrOutput, bcrOutput) %>%
         lapplyOnAll(function(df) {
-            df[df[[rawBarcodeColname]] %in% doubletBarcodes, ]
+            df[df$barcode %in% doubletBarcodes, ]
         }) %>%
-        lapply(dplyr::bind_rows) %>%
-        purrr::reduce(autoFullJoin)
+        lapply(dplyr::bind_rows)
+
+    RbindedTcrBcrDoublets[[1]]$contigType <- "TCR"
+    RbindedTcrBcrDoublets[[2]]$contigType <- "BCR"
+    
+    RbindedTcrBcrDoublets %>%
+        purrr::reduce(autoFullJoin) %>%
+        dplyr::mutate(contigType = factor(contigType))
 }
 
-tempColnameForDfList <- function(dfList, baseName = "temp") {
-    colnameSet <- unique(unlist(lapply(dfList, colnames)))
-    tail(make.unique(c(colnameSet, baseName)), 1)
+getContigDoubletBarcodes <- function(tcrOutput, bcrOutput) {
+    list(tcrOutput, bcrOutput) %>%
+        lapplyOnAll(function(df) unique(df$barcode)) %>%
+        lapply(purrr::list_flatten) %>%
+        purrr::reduce(intersect)
 }
 
 lapplyOnAll <- function(listOfLists, fun) {
     lapply(listOfLists, function(x) lapply(x, fun))
 }
 
-makeEmptyIntersectionDf <- function(...) {
-    purrr::reduce(list(...), function(df1, df2) {
-        autoFullJoin(df1[0, ], df2[0, ])
-    })
-}
-
 autoFullJoin <- function(df1, df2) {
     suppressMessages(dplyr::full_join(df1, df2))
-}
-
-extractBarcodeStrings <- function(inputStrings) {
-    matches <- unlist(lapply(inputStrings, function(x) {
-        regmatches(x, gregexpr("[a-zA-Z_]+_[ATGC]+-\\d+", x))
-    }))
-    matches[matches != ""]
 }
---
+++ tests/testthat/test-getContigDoublets.R
@@ -0,0 +1,27 @@
+test_that("getContigDoublets works for no doublets", {
+
+    tcr <- getdata("combineContigs", "combineTCR_list_expected")
+
+    # create a BCR list from testdata with no doublets
+    bcr <- getdata("combineContigs", "combineBCR_list_expected")
+    bcr <- list(bcr[[1]][1:10, ], bcr[[1]][20:30, ], bcr[[1]][100:110, ])
+    names(bcr) <- names(tcr)
+
+    expected_no_doublet_output <- structure(
+        list(
+            barcode = character(0), sample = character(0),
+            TCR1 = character(0), cdr3_aa1 = character(0),
+            cdr3_nt1 = character(0), TCR2 = character(0),
+            cdr3_aa2 = character(0), cdr3_nt2 = character(0),
+            CTgene = character(0), CTnt = character(0), CTaa = character(0),
+            CTstrict = character(0), IGH = character(0), IGLC = character(0),
+            contigType = structure(
+                integer(0), levels = c("BCR", "TCR"), class = "factor"
+            )
+        ),
+        row.names = integer(0),
+        class = "data.frame"
+    )
+
+    expect_identical(getContigDoublets(tcr, bcr), expected_no_doublet_output)
+})
