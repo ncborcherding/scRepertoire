14d181607bc1427c9bfbef1f6f00196846b7d170
Qile0317 <qile0317@gmail.com>
2024-10-22 18:57:57 -0700

    Revert "unfinished changes for parseBCR update"
    This reverts commit 3280b9a7a256b52a05a51ddae4c378839567cfd9.

--- src/scRepHelper.h
+++ src/scRepHelper.h
@@ -1,14 +1,10 @@
-#pragma once
-
 #include <Rcpp.h>
 #include <vector>
 #include <string>
 #include <unordered_map>
 
-#define BarcodeIndciesMap std::unordered_map<std::string, std::vector<int>>
-
 class scRepHelper {
-public:
+public: 
     static double sum(std::vector<double>& v) {
         double n = 0;
         for (double num : v) {
@@ -31,27 +27,23 @@ public:
         return convertZerosToNA(v, (int) v.size());
     }
 
-    static std::vector<std::vector<int>> constructBarcodeIndex(
-        std::vector<std::string>& conDfBarcodes, std::vector<std::string> data2Barcodes
-    ) {
-        std::vector<std::vector<int>> outputBarcodeIndex (conDfBarcodes.size());
-        BarcodeIndciesMap data2BarcodeIndiciesMap = stringToIndiciesMap(data2Barcodes);
-
-        for (int i = 0; i < (int) conDfBarcodes.size(); i++) {
-            std::string& barcode = conDfBarcodes[i];
-            if (data2BarcodeIndiciesMap.find(barcode) != data2BarcodeIndiciesMap.end()) {
-                outputBarcodeIndex[i] = data2BarcodeIndiciesMap[barcode];
-            }
-        }
-        return outputBarcodeIndex;
-    }
-
     static std::unordered_map<std::string, std::vector<int>> stringToIndiciesMap(
         std::vector<std::string>& v
     ) {
         std::unordered_map<std::string, std::vector<int>> map;
         for (int i = 0; i < (int) v.size(); i++) {
             map[v[i]].push_back(i);
+        }
+        return map;
+    }
+
+    // remove when constructConDfAndParseBCR is done
+    static std::unordered_map<std::string, std::vector<int>> stringToRIndiciesMap(
+        std::vector<std::string>& v
+    ) {
+        std::unordered_map<std::string, std::vector<int>> map;
+        for (int i = 0; i < (int) v.size(); i++) {
+            map[v[i]].push_back(i + 1);
         }
         return map;
     }
