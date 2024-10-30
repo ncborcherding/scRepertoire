7aef5043356c9d010ba42ec370dee4a8e63784d1
Qile0317 <qile0317@gmail.com>
2024-10-29 19:00:59 -0700

    add is.count impoty

--- R/scRepertoire-package.R
+++ R/scRepertoire-package.R
@@ -4,5 +4,6 @@
 ## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib scRepertoire, .registration = TRUE
+#' @importFrom assertthat assert_that is.count is.flag is.string
 ## usethis namespace: end
 NULL
